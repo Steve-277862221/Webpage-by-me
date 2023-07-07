2c62689554421dd5a67f53d0796056904aaf3ab6
Steve-277862221 <131404478+Steve-277862221@users.noreply.github.com>
2023-07-07 16:10:11 +0530

    Add files via upload

---
+++ JS-1.js
@@ -0,0 +1,11 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+/*
+ * Click https://www.web-1.web/sysfilesys/open=?/syscampos/open.html/root.index to do nothing
+ * Open https://www.web-1web/home to open homepage
+ * Type url https://web-1.com/home to open html
+ */
+
+
